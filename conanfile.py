d27768f2cc9ee92cc935262a823f18ef77261838
Sohail Siadat <sohale@gmail.com>
2024-03-25 17:58:05 +0000

    setting up conan: 3: conanfile (2)

--- conanfile.py
+++ conanfile.py
@@ -9,10 +9,24 @@ cmake
 from conans import ConanFile, CMake
 
 class Neopiler(ConanFile):
+
+   name = " NeoPiler"
+   version = "0.1"
+
    settings = "os", "compiler", "build_type", "arch"
 
-   requires = ""   # comma-separated list of requirements
-   generators = "cmake", "gcc", "txt"  # todo: make it clang
+   requires = "antlr4/4.13.1", "antlr4-cppruntime/4.13.1"
+
+   # generators = "cmake", "gcc", "txt"  # todo: make it clang
+   generators = "cmake"
+
+   default_options = {"antlr4:shared": True, "antlr4-cppruntime:shared": True}
+
 
    def imports(self):
       pass
+
+   def build(self):
+      cmake = CMake(self)
+      cmake.configure()
+      cmake.build()
