9553956260b44359aae9e8e26e0a4bda9eea8963
Sohail Siadat <sohale@gmail.com>
2024-03-25 18:34:56 +0000

    putting too much sauce in the conanfile

--- conanfile.py
+++ conanfile.py
@@ -18,12 +18,34 @@ class Neopiler(ConanFile):
    requires = "antlr4/4.13.1", "antlr4-cppruntime/4.13.1"
 
    # generators = "cmake", "gcc", "txt"  # todo: make it clang
-   generators = "cmake"
+   generators = "cmake", "txt"
+   # See various generators: https://docs.conan.io/1/reference/generators.html
+   #    "cmake":
+   #    "cmake_find_package": This generator is suitable for CMake-based projects
+   #    "txt": generic: produces conanbuildinfo.txt for human use (paths, flags, etc)
+   #    "gcc": manually / preexisting build using gcc
+   #    "CMakeToolchain": ? See https://docs.conan.io/2/reference/tools/cmake/cmaketoolchain.html#conan-tools-cmaketoolchain
+   #    Multiple: You can use multiple to generate multiple files
+   #    Alternatives: Meson (for simplicity and speed), CMake, SCons, Ninja (via Cmake), "cmake_find_package" (the cmake for ninja), Make (can call macke commands in `build()`), etc
+   #    Not supported: Bazel (not supported in Conan out of the box),
+
+
+   options = {"fPIC": [True, False]}
 
    default_options = {
+
       "antlr4:shared": True,
       "antlr4-cppruntime:shared": True,
+
+      "fPIC": True,
    }
+
+   # eq. to : generators = "CMakeToolchain" (?)
+   # def generate(self):
+   #     tc = CMakeToolchain(self)
+   #     tc.variables["MYVAR"] = "MYVAR_VALUE"
+   #     tc.preprocessor_definitions["MYDEFINE"] = "MYDEF_VALUE"
+   #     tc.generate()
 
    def configure(self):
       if self.settings.compiler == "clang":
@@ -39,11 +61,11 @@ class Neopiler(ConanFile):
       # assert self.settings.arch == "x86_64", "Only x86_64 is supported"
 
 
-   def imports(self):
-      pass
+   #def imports(self):
+   #   pass
 
    def build(self):
-      cmake = CMake(self)
+      cmake = CMake(self, generator="Ninja")
       # `#DEFINE` definitions:
       cmake.definitions["CMAKE_CXX_STANDARD"] = "20"  # Necessary for cmake
       cmake.configure()
